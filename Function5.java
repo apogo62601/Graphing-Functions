d664162fa8818afaefead82e34498a60bde19141
Aelina Pogosian <60406142+apogo62601@users.noreply.github.com>
2020-04-22 17:51:01 -0400

    Add files via upload

---
+++ Function5.java
@@ -0,0 +1,13 @@
+
+public class Function5 extends Function {
+	@Override
+	public double fnValue(double x) {
+		//  complete sin(1/x)
+		return Math.sin(1 / x);
+	}
+
+	public String toString () {
+		//  complete sin(1/x)
+		return "sin(1/x)";
+	}
+}
